78c359139c34abb29d4fb70305651c021ff261bf
salimov-bilol <salimovb1999@gmail.com>
2021-03-05 22:43:55 +0500

    Initial commit

---
+++ src/main/java/com/harvard/demo/MyController.java
@@ -0,0 +1,13 @@
+package com.harvard.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
